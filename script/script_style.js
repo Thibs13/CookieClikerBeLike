bde0a9d1ea369e27f27c048f76b04aaf6c105257
Thibault <caumontthibault1313@gmail.com>
2024-12-16 13:47:16 +0100

    maj

--- script/script_style.js
+++ script/script_style.js
@@ -7,11 +7,27 @@ function pause(milliseconds) {
     cookieImg.classList.remove("effetClickCookie");
   }
 
+const blocSettings = document.querySelector(".bloc-settings")
+const btnList = document.querySelector(".list-button")
 const cookieImg = document.querySelector("#cookieImg")
 const nbreCookies = document.querySelector("#nbreCookies")
+const btnChef = document.querySelector(".chef")
 var compteurCookie = 0;
+var puissanceClick = 1;
 cookieImg.addEventListener("click",()=>{
     effetCookie();
-    compteurCookie++;
+    compteurCookie+=puissanceClick;
     nbreCookies.innerHTML =`${compteurCookie}`;
+})
+
+btnList.addEventListener("click",()=>{
+  blocSettings.classList.toggle("inv")
+  cookieImg.classList.toggle("inv")
+})
+btnChef.addEventListener("click",()=>{
+  if(compteurCookie>=50){
+    compteurCookie-=50;
+    puissanceClick+=1;
+    nbreCookies.innerHTML =`${compteurCookie}`;
+  }
 })
