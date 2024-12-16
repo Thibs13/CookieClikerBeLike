456b0bfd0df8396ebbb5f9b9a978e201289211d1
Thibault <caumontthibault1313@gmail.com>
2024-12-16 15:08:42 +0100

    maj

--- script/script_style.js
+++ script/script_style.js
@@ -11,9 +11,21 @@ const blocSettings = document.querySelector(".bloc-settings")
 const btnList = document.querySelector(".list-button")
 const cookieImg = document.querySelector("#cookieImg")
 const nbreCookies = document.querySelector("#nbreCookies")
+
 const btnChef = document.querySelector(".chef")
+const coutChef = document.querySelector("#coutChef")
+var nombreChef = 0;
+var coutChefComp = 50;
+
+const btnUsine = document.querySelector(".usine")
+const coutUsine = document.querySelector("#coutUsine")
+var nombreUsine = 0;
+var coutUsineComp = 1000;
+
+
 var compteurCookie = 0;
 var puissanceClick = 1;
+
 cookieImg.addEventListener("click",()=>{
     effetCookie();
     compteurCookie+=puissanceClick;
@@ -25,9 +37,23 @@ btnList.addEventListener("click",()=>{
   cookieImg.classList.toggle("inv")
 })
 btnChef.addEventListener("click",()=>{
-  if(compteurCookie>=50){
-    compteurCookie-=50;
+  if(compteurCookie>=coutChefComp){
+    compteurCookie-=coutChefComp;
     puissanceClick+=1;
+    nombreChef++;
+    coutChefComp=Math.round(50*(1.25**nombreChef));
+    coutChef.innerHTML= `Coût : ${coutChefComp}`
+    nbreCookies.innerHTML =`${compteurCookie}`;
+  }
+})
+
+btnUsine.addEventListener("click",()=>{
+  if(compteurCookie>=coutUsineComp){
+    compteurCookie-=coutUsineComp;
+    puissanceClick+=25;
+    nombreUsine++;
+    coutUsineComp=Math.round(1000*(1.2**nombreUsine));
+    coutUsine.innerHTML= `Coût : ${coutUsineComp}`
     nbreCookies.innerHTML =`${compteurCookie}`;
   }
 })
